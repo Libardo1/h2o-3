333d0ad927cc7ac27ebb926ed6488aec4a5a4cd4
wendycwong <wendycwong@yahoo.com>
2018-06-21 20:21:15 -0700

    Fixed test.

--- h2o-core/src/test/java/water/fvec/NewChunkSpeedTest.java
+++ h2o-core/src/test/java/water/fvec/NewChunkSpeedTest.java
@@ -21,13 +21,12 @@ public class NewChunkSpeedTest extends TestUtil {
   double tolerance = 1e-10;
   int numberLoops=2;
 
-
-  @Test public void testParseBigDoubles(){
+  @Test public void testParseDoublesConst(){
     double startTime = System.currentTimeMillis();
     for (int index=0; index<numberLoops; index++)
-      testsForDoubles(false, true, false);
+      testsForDoubles(true, false, false);
     double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for big doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
+    Log.info("New Chunk test for constant doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
 
   @Test public void testParseBigDoublesConst(){
@@ -38,40 +37,13 @@ public class NewChunkSpeedTest extends TestUtil {
     Log.info("New Chunk test for big constant doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
 
-  @Test public void testParseDoubles(){
+  @Test public void testParseFloatsConst(){
     double startTime = System.currentTimeMillis();
     for (int index=0; index<numberLoops; index++)
-      testsForDoubles(false, false, false);
+      testsForDoubles(true, false, true);
     double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
+    Log.info("New Chunk test for constant floats:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
-
-  @Test public void testParseDoublesConst(){
-    double startTime = System.currentTimeMillis();
-    for (int index=0; index<numberLoops; index++)
-      testsForDoubles(true, false, false);
-    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for constant doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
-  }
-
-  // todo: This should be changed to test after Spencer PR is in.
-  @Test
-  public void testParseLong(){
-    double startTime = System.currentTimeMillis();
-    for (int index=0; index<numberLoops; index++)
-      testsForLongs(false);
-    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for longs:", " time(s) taken for "+numberLoops+" is "+endTime);
-  }
-
-  @Test public void testParseLongConsts(){
-    double startTime = System.currentTimeMillis();
-    for (int index=0; index<numberLoops; index++)
-      testsForLongs(true);
-    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for constant longs:", " time(s) taken for "+numberLoops+" is "+endTime);
-  }
-
 
   @Test public void testParseFloats(){
     double startTime = System.currentTimeMillis();
@@ -81,12 +53,20 @@ public class NewChunkSpeedTest extends TestUtil {
     Log.info("New Chunk test for floats:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
 
-  @Test public void testParseFloatsConst(){
+  @Test public void testParseDoubles(){
     double startTime = System.currentTimeMillis();
     for (int index=0; index<numberLoops; index++)
-      testsForDoubles(true, false, true);
+      testsForDoubles(false, false, false);
     double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
-    Log.info("New Chunk test for constant floats:", " time(s) taken for "+numberLoops+" loops is "+endTime);
+    Log.info("New Chunk test for doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
+  }
+
+  @Test public void testParseBigDoubles(){
+    double startTime = System.currentTimeMillis();
+    for (int index=0; index<numberLoops; index++)
+      testsForDoubles(false, true, false);
+    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
+    Log.info("New Chunk test for big doubles:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
 
   @Test public void testParseInteger(){
@@ -105,6 +85,49 @@ public class NewChunkSpeedTest extends TestUtil {
     Log.info("New Chunk test for constant integer:", " time(s) taken for "+numberLoops+" loops is "+endTime);
   }
 
+
+  // todo: This should be changed to test after Spencer PR is in.
+  @Test
+  public void testParseLong(){
+    double startTime = System.currentTimeMillis();
+    for (int index=0; index<numberLoops; index++)
+      testsForLongs(false);
+    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
+    Log.info("************************************************");
+    Log.info("New Chunk test for longs:", " time(s) taken for "+numberLoops+" is "+endTime);
+  }
+
+  @Test public void testParseLongConsts(){
+    double startTime = System.currentTimeMillis();
+    for (int index=0; index<numberLoops; index++)
+      testsForLongs(true);
+    double endTime = (System.currentTimeMillis()-startTime)*0.001;  // change time to seconds
+    Log.info("New Chunk test for constant longs:", " time(s) taken for "+numberLoops+" is "+endTime);
+  }
+
+  @Test
+  public void testParseDataFromFiles() {
+    String[] filenames = new String[]{"smalldata/jira/floatVals.csv", "smalldata/jira/integerVals.csv",
+            "smalldata/jira/longVals.csv", "smalldata/jira/doubleVals.csv", "smalldata/jira/bigDoubleVals.csv"};
+    int numLoops = 5 * numberLoops;
+
+    for (int index = 0; index < filenames.length; index++) {
+      double startTime = System.currentTimeMillis();
+      for (int loop = 0; loop < numLoops; loop++) {
+        Scope.enter();
+        try {
+          Frame f = parse_test_file(filenames[index]);
+          assertTrue(f.numRows() == 100000);
+          Scope.track(f);
+        } finally {
+          Scope.exit();
+        }
+      }
+      double endTime = (System.currentTimeMillis() - startTime) * 0.001;  // change time to seconds
+      Log.info("*******************************************************");
+      Log.info("Parsing: " + filenames[index] + " time(s) taken for " + numLoops + " loops is " + endTime);
+    }
+  }
 
   public void testsForLongs(boolean forConstants) {
     Scope.enter();
